72c7c95467d8c0d5d21f027eb1ab99df2d6563f3
ship <594117065@qq.com>
2023-09-02 08:43:29 +0800

    开始函数学习

--- 02_datatype/dict_demo.py
+++ 02_datatype/dict_demo.py
@@ -1,3 +1,90 @@
 """
-字典
+【字典】
+查找速度快，无论是 10 个元素还是 10 万个元素，查找速度都一样
+key 必须使用不可变类型，如果使用可变类型（比如：list）会报错
+缺点：内存占用大
 """
+
+print('\n----------- 字典演示 -----------')
+d = {
+    'Alice': 45,
+    'Bob': 60,
+    'Candy': 75,
+    'David': 86,
+    'Ellena': 49,
+    'Gaven': 86
+}
+print(d)
+
+print('\n----------- 从字典中获取元素 - dict[key] -----------')
+print('Alice 的成绩：', d['Alice'])
+try:
+    print(d['pig'])
+except KeyError as e:
+    print('key 不存在，', repr(e))
+
+print('\n----------- 从字典中获取元素（避免 KeyError） - dict.get(key) -----------')
+print(d.get('pig'))
+
+print('\n----------- 字典的更新操作：已存在就更新，不存在就添加 -----------')
+d['Alice'] = 69
+d['ppp'] = 99
+print(d)
+
+print('\n----------- 字典的删除操作：pop() / pop(key) -----------')
+print(fr'''
+删除 ['ppp'], pop() 方法会返回被删除元素的 value：{d.pop('ppp')}
+''')
+
+print('\n----------- 判断 key 是否存在于字典中 -----------')
+if 'ppp' in d.keys():
+    print(d['ppp'])
+else:
+    print('{} not in tuple'.format('ppp'))
+
+print('\n----------- 遍历字典 -----------')
+# for 循环遍历字典的所有 key
+for key in d.keys():
+    print(key)
+# for 循环遍历字典的所有 value
+for value in d.values():
+    print(value)
+# for 循环遍历字典的所有 key，并找出 value 大于 60 的
+for key in d:
+    value = d[key]
+    if value > 60:
+        print(key, value)
+# for 循环遍历字典的所有 key，并找出 value 大于 60 的
+for key, value in d.items():
+    if value > 60:
+        print(key, value)
+
+print('\n----------- 字典的长度 -----------')
+print(len(d))
+print(len(d.keys()))
+print(len(d.values()))
+
+print('\n----------- 清空 -  clear() -----------')
+d.clear()
+print(d)
+
+print('\n----------- 示例 - 使用字典存放 3 个同学的多次成绩 -----------')
+d = dict()
+# 用 list 来存放每位同学的成绩
+d['Alice'] = []
+d['Bob'] = []
+d['Candy'] = []
+# 开始填充成绩
+d['Alice'].append(50)
+d['Alice'].append(61)
+d['Alice'].append(66)
+d['Bob'].append(80)
+d['Bob'].append(61)
+d['Bob'].append(66)
+d['Candy'].append(88)
+d['Candy'].append(75)
+d['Candy'].append(90)
+# 输出打印每个同学的成绩
+for key, value in d.items():
+    for index, score in enumerate(value, 1):
+        print('{} 第 {} 次成绩：{}'.format(key, index, score))
--- 02_datatype/set_demo.py
+++ 02_datatype/set_demo.py
@@ -1,3 +1,46 @@
 """
-集合
+【集合】
+无序的、不允许重复元素
 """
+
+print('\n----------- 创建一个 set, 注意和 list 的区别 -----------')
+s = {1, 4, 3, 2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6}
+print('a set - ', s)
+L = [1, 2, 3, 4, 6, 6, 6]
+print('a list - ', L)
+
+print('\n----------- 读取 set -----------')
+for item in s:
+    print(item)
+
+print('\n----------- 添加 set - add 和 update -----------')
+names = {'Alice', 'Bob', 'Candy', 'David', 'Ellena'}
+names.add('pppp')
+names.update({'54pig', '54pink', '54ppppp'})
+print(names)
+
+print('\n----------- 删除 set 元素 - remove() -----------')
+L = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+S = {1, 3, 5, 7, 9}
+for item in L:
+    if item in S:
+        S.remove(item)
+    else:
+        S.add(item)
+print(S)
+
+print('\n----------- 删除 set 元素（元素不存在也不会报错） - discard() -----------')
+names = {'Alice', 'Bob', 'Candy', 'David', 'Ellena'}
+names.discard('fj4i23qu5o32hr')
+print(names)
+
+print('\n----------- 清理 set - clear() -----------')
+names.clear()
+print(names)
+
+print('\n----------- 子集、超集、重合 -----------')
+s1 = {1, 2, 3, 4, 5}
+s2 = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+print('s1 是 s2 的子集（s1 的元素全部存在于 s2 中）：', s1.issubset(s2))
+print('s2 是 s1 的超集（s2 包含了 s1 的所有元素）：', s2.issuperset(s1))
+print('s1 和 s2 中有重复元素：', not s1.isdisjoint(s2))
