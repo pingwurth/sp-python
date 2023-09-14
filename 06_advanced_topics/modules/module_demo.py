7babc8491869afd40278909e78bbc650bf969bd9
ship <594117065@qq.com>
2023-09-15 02:21:30 +0800

    sys和math模块

--- 06_advanced_topics/modules/module_demo.py
+++ 06_advanced_topics/modules/module_demo.py
@@ -1,13 +1,26 @@
 import math
 import os
 import sys
+import time
 
-print(math.fabs(-123.0))
+"""
+math 模块演示
+"""
+print(f'math 模块 ---------- 取绝对值 {math.fabs(-123.0)}')
+print(f'math 模块 ---------- 向下取整 {math.floor(-2.5)}')
+print(f'math 模块 ---------- 向上取整 {math.ceil(-2.5)}')
+print(f'math 模块 ---------- 指数运算：2 的平方 = {math.pow(2, 2)}')
+print(f'math 模块 ---------- 平方根运算：√16 = {math.sqrt(16)}')
+print(f'math 模块 ---------- 拆分小数和整数：3.1415926 = {math.modf(3.1415926)}')
+print(f'math 模块 ---------- 列表元素累加和：[1, 2, 3] = {math.fsum([1, 2, 3])}')
+# math.copysign(a, b) 的功能是将参数 b 的正负符号复制给第一个数
+print(f'math 模块 ---------- 正负符号传递：{math.copysign(2, -1)}')
+print(f'math 模块 ---------- 圆周率 Π = {math.pi}')
+print(f'math 模块 ---------- 自然对数常量 e = {math.e}')
 
 """
 os 模块演示
 """
-
 print('\n----------- os 模块 - 实现文件拷贝 -----------')
 
 
@@ -77,14 +90,33 @@ def list_dir(directory):
 
 list_dir('tmp')
 
-
-
-
-
 """
 sys 模块演示
 """
+print('\n----------- sys 模块 - 命令行参数 -----------')
+print(sys.argv)  # sys.argv 是一个数组
+
+print('\n----------- sys 模块 - 示例：打印下载进度 -----------')
+for rate in range(101):
+    text = 'Downloading %d%%' % rate
+    sys.stdout.write(text)
+    sys.stdout.write('\r')  # 输出 '\r'，将光标移动到行首
+    time.sleep(0.01)
+
+print('\n----------- sys 模块 - 系统路径（Python 库的路径列表） -----------')
+print(sys.path)
+
+print('\n----------- sys 模块 - 输出 Python 版本、系统版本 -----------')
+print(sys.version)
+print(sys.platform)
 
 print('\n----------- sys 模块 - 退出程序 -----------')
 # 0 表示正常退出，其他值表示异常退出
 sys.exit(0)
+
+print('\n----------- sys 模块 - 标准输入、标准输出、标准错误输出 -----------')
+line = sys.stdin.readline()
+sys.stdout.write(line)
+sys.stderr.write(line)
+
+
